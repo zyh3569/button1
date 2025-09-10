f12ff832aa9c3e404e715373d724d2e381324f1b
Yeonseung Ryu <ysryu@mju.ac.kr>
2025-09-10 10:38:08 +0900

    modify my hakbun and name

--- button1.py
+++ button1.py
@@ -3,7 +3,7 @@ from tkinter import *
 window =Tk()
 
 b1 = Button(window, text="Yeonseung Ryu")
-b2 = Button(window, text="60231010")
+b2 = Button(window, text="60241010")
 b1.grid(row=0, column=0)
 b2.grid(row=1, column=1)
 
